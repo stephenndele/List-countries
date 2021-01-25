7bd7b39d5f29f608314241c90736ef23693fcc13
Stephen Ndele <stephenndele09346@gmail.com>
2021-01-25 22:37:29 +0300

    fect countries

---
+++ index.js
@@ -0,0 +1,16 @@
+const countriesAPI = 'https://restcountries.eu/rest/v2/all'
+const catsAPI = 'https://api.thecatapi.com/v1/breeds'
+
+fetch(countriesAPI)
+  .then(response => response.json())
+  .then(data => {
+    console.log(data)
+  })
+  .catch(error => console.log(error))
+  fetch(catsAPI)
+  .then(response => response.json())
+  .then(data => {
+    console.log(data)
+  })
+
+  
